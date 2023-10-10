258ef452e61bc0e7b4473eb9fcb8bc4a47ca4025
Varun <143994029+VarunGithub9@users.noreply.github.com>
2023-10-10 17:03:24 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,10 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "VarunGithub9",
+    username: "https://github.com/VarunGithub9",
+  },
+
 ];
